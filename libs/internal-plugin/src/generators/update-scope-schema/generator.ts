e8423721ef3ea3bb159d9a368bbac8525af25eb8
Nathan May <nathaniel.may22@gmail.com>
2022-10-18 11:47:46 -0700

    scope generator

--- libs/internal-plugin/src/generators/update-scope-schema/generator.ts
+++ libs/internal-plugin/src/generators/update-scope-schema/generator.ts
@@ -1,23 +1,57 @@
 import {
-  formatFiles, Tree, updateJson
+  formatFiles, getProjects, ProjectConfiguration, Tree, updateJson
 } from '@nrwl/devkit';
-import { UpdateScopeSchemaGeneratorSchema } from './schema';
 
-interface NormalizedSchema extends UpdateScopeSchemaGeneratorSchema {
-  projectName: string;
-  projectRoot: string;
-  projectDirectory: string;
-  parsedTags: string[];
+
+
+export default async function (tree: Tree) {
+
+  const projects = getProjects(tree)
+  const scopes = getScopes(projects)
+  updateSchema(tree, scopes)
+  updateSchemaInterface(tree, scopes)
+  await formatFiles(tree);
+}
+
+function updateSchema(tree: Tree, scopes: string[]) {
+  updateJson(
+    tree,
+    'libs/internal-plugin/src/generators/util-lib/schema.json',
+    (schemaJson) => {
+      schemaJson.properties.directory['x-prompt'].items = scopes.map(
+        (scope) => ({
+          value: scope,
+          label: scope,
+        })
+      );
+      return schemaJson;
+    }
+  );
+}
+function updateSchemaInterface(tree: Tree, scopes: string[]) {
+  const joinScopes = scopes.map((s) => `'${s}'`).join(' | ');
+  const interfaceDefinitionFilePath =
+    'libs/internal-plugin/src/generators/util-lib/schema.d.ts';
+  const newContent = `export interface UtilLibGeneratorSchema {
+  name: string;
+  directory: ${joinScopes};
+}`;
+  tree.write(interfaceDefinitionFilePath, newContent);
 }
 
 
-
-export default async function (tree: Tree, options: UpdateScopeSchemaGeneratorSchema) {
-  updateJson(tree, 'nx.json', (json) => ({
-    ...json,
-    defaultProject: 'api',
-  }))
-
-
-  await formatFiles(tree);
+function getScopes(projectMap: Map<string, ProjectConfiguration>) {
+  const projects: ProjectConfiguration[] = Array.from(projectMap.values());
+  const allScopes: string[] = projects
+    .map((project) =>
+      project.tags
+        // take only those that point to scope
+        .filter((tag: string) => tag.startsWith('scope:'))
+    )
+    // flatten the array
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    // remove prefix `scope:`
+    .map((scope: string) => scope.slice(6));
+  // remove duplicates
+  return Array.from(new Set(allScopes));
 }
